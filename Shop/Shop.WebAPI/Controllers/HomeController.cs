b5e4a15addf9cd0ffad8082cc36e5c45fd66e7c3
HannaFilon <annfilon16@gmail.com>
2021-07-01 16:33:27 +0300

    Create project, tasks form card 4

---
+++ Shop/Shop.WebAPI/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace Shop.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetInfo()
+        {
+            return Ok(JsonSerializer.Serialize("Hello World!"));
+        }
+    }
+}
