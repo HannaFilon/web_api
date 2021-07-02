9491f246995c0fb578ac2a817d5409144c9e93f6
HannaFilon <annfilon16@gmail.com>
2021-07-02 15:28:00 +0300

    Changes made: got rid of additional code

--- Shop/Shop.DAL.Core/ShopContext.cs
+++ Shop/Shop.DAL.Core/ShopContext.cs
@@ -1,14 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using Shop.DAL.Core.Entities;
 
 namespace Shop.DAL.Core
 {
     public class ShopContext: IdentityDbContext
     {     
-            public DbSet<Product> Products { get; set; }
-            public DbSet<Category> Categories { get; set; }
-
+           
             public ShopContext(DbContextOptions<ShopContext> options) : base(options) { }    
     }   
 }
