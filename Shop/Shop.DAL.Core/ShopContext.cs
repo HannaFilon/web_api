db3b7a89da3ef3755c3ec620352358c9cfbf6ac0
HannaFilon <78898065+HannaFilon@users.noreply.github.com>
2021-07-02 16:13:10 +0300

    Card 4 - Create initial structure. Backend (#2)
    * Create .gitignore file
    
    * Create project, tasks from card 4 completed
    
    * Changes made: got rid of additional code

---
+++ Shop/Shop.DAL.Core/ShopContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Shop.DAL.Core
+{
+    public class ShopContext: IdentityDbContext
+    {     
+           
+            public ShopContext(DbContextOptions<ShopContext> options) : base(options) { }    
+    }   
+}
