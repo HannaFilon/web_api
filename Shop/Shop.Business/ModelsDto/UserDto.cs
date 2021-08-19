acdd9b3af93df82abcaa4c2890130a25d3922706
HannaFilon <annfilon16@gmail.com>
2021-08-19 11:11:17 +0300

    Add Caching, GzipCompression, edit Json Serialization/Deserialization options

--- Shop/Shop.Business/ModelsDto/UserDto.cs
+++ Shop/Shop.Business/ModelsDto/UserDto.cs
@@ -1,13 +1,17 @@
-﻿namespace Shop.Business.ModelsDto
+﻿using System.Text.Json.Serialization;
+
+namespace Shop.Business.ModelsDto
 {
     public class UserDto
     {
+        [JsonIgnore]
         public string Id { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
         public string Role { get; set; }
         public string AddressDelivery { get; set; }
         public string PhoneNumber { get; set; }
+        [JsonIgnore]
         public string SecurityStamp { get; set; }
     }
 }
