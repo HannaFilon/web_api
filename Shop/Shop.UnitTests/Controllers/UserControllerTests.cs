7cea4e3a5acf084b5b70f5dad5168dbdb3582198
HannaFilon <annfilon16@gmail.com>
2021-08-26 10:58:51 +0300

    Edit tests

--- Shop/Shop.UnitTests/Controllers/UserControllerTests.cs
+++ Shop/Shop.UnitTests/Controllers/UserControllerTests.cs
@@ -14,25 +14,25 @@ using Shop.Business.Models;
 using Shop.Business.ModelsDto;
 using Shop.WebAPI.Auth;
 using Shop.WebAPI.Controllers;
+using System.Collections.Generic;
 
 namespace Shop.UnitTests.Controllers
 {
     public class UserControllerTests
     {
-        private UserController SetUserController(string userId)
+        private static readonly UserController _userController;
+        private static readonly IAuthManager _authManager;
+
+        static UserControllerTests()
         {
+            _authManager = A.Fake<IAuthManager>();
+
             var fakeUserService = A.Fake<IUserService>();
-            var fakeAuthManager = A.Fake<IAuthManager>();
             var fakeMapper = A.Fake<IMapper>();
             var fakeServiceProvider = A.Fake<IServiceProvider>();
             var fakeAuthService = A.Fake<IAuthenticationService>();
             var fakeTicket = A.Fake<AuthenticationTicket>();
             var fakeContext = A.Fake<HttpContext>();
-
-            var jwtToken = new JwtSecurityToken(claims: new Claim[]
-            {
-                new("id", userId)
-            });
 
             A.CallTo(() => fakeUserService.UpdateUser(A<string>._, A<UserModel>._))
                  .Returns(new UserDto());
@@ -40,8 +40,8 @@ namespace Shop.UnitTests.Controllers
                 .Returns(Task.FromResult(IdentityResult.Success));
             A.CallTo(() => fakeMapper.Map<UserModel>(A<UserDto>._))
                 .Returns(new UserModel());
-            A.CallTo(() => fakeAuthManager.DecodeJwtToken(A<string>._))
-                .Returns(jwtToken);
+            A.CallTo(() => _authManager.DecodeJwtToken(A<string>._))
+                .Returns(new JwtSecurityToken());
             A.CallTo(() => fakeContext.RequestServices)
                 .Returns(fakeServiceProvider);
             A.CallTo(() => fakeServiceProvider.GetService(typeof(IAuthenticationService)))
@@ -49,116 +49,119 @@ namespace Shop.UnitTests.Controllers
             A.CallTo(() => fakeAuthService.AuthenticateAsync(fakeContext, A<string>._))
                 .Returns(AuthenticateResult.Success(fakeTicket));
 
-            var userController = new UserController(fakeUserService, fakeMapper, fakeAuthManager)
+            _userController = new UserController(fakeUserService, fakeMapper, _authManager)
             {
                 ControllerContext = { HttpContext = fakeContext }
             };
-
-            return userController;
         }
-
 
         [Fact]
         public async void UpdateUser_ReturnUpdatedModel()
         {
-            var userController = SetUserController(Guid.NewGuid().ToString());
+            StubAuthManager(Guid.NewGuid().ToString());
 
-            var result = await userController.UpdateUser(new UserModel());
+            var result = await _userController.UpdateUser(new UserModel());
 
             var okResult = result as OkObjectResult;
             Assert.NotNull(okResult);
-            var modelResult = okResult.Value as UserModel;
-            Assert.NotNull(modelResult);
+            Assert.IsType<UserModel>(okResult.Value);
+            Assert.NotNull(okResult.Value);
         }
 
-        [Fact]
-        public async void UpdateUser_ReturnsBadRequest_WrongToken()
+        [Theory]
+        [MemberData(nameof(UpdateUser_ReturnsBadRequestTestData))]
+        public async void UpdateUser_ReturnsBadRequest(string userId, UserModel userModel, string expectedError)
         {
-            var userController = SetUserController(string.Empty);
+            StubAuthManager(userId);
 
-            var result = await userController.UpdateUser(new UserModel());
+            var result = await _userController.UpdateUser(userModel);
 
             var badRequestResult = result as BadRequestObjectResult;
             Assert.NotNull(badRequestResult);
-            var stringValue = badRequestResult.Value as string;
-            Assert.Equal("This method is unavailable.", stringValue);
-        }
-
-        [Fact]
-        public async void UpdateUser_ReturnsBadRequest_NullUserModel()
-        {
-            var userController = SetUserController(Guid.NewGuid().ToString());
-
-            var result = await userController.UpdateUser(null);
-
-            var badRequestResult = result as BadRequestObjectResult;
-            Assert.NotNull(badRequestResult);
-            var stringValue = badRequestResult.Value as string;
-            Assert.Equal("Not enough parameters to update user.", stringValue);
+            Assert.IsType<string>(badRequestResult.Value);
+            Assert.Equal(expectedError, badRequestResult.Value);
         }
 
         [Fact]
         public async void UpdatePassword_Return204()
         {
-            var userController = SetUserController(Guid.NewGuid().ToString());
+            StubAuthManager(Guid.NewGuid().ToString());
 
-            var result = await userController.UpdatePassword(new PasswordUpdateModel());
+            var result = await _userController.UpdatePassword(new PasswordUpdateModel());
 
             var statusCode = result as StatusCodeResult;
             Assert.NotNull(statusCode);
             Assert.Equal(204, statusCode.StatusCode);
         }
 
-        [Fact]
-        public async void UpdatePassword_ReturnsBadRequest_WrongToken()
+        [Theory]
+        [MemberData(nameof(UpdatePassword_ReturnsBadRequestTestData))]
+        public async void UpdatePassword_ReturnsBadRequest(string userId, PasswordUpdateModel passwordUpdateModel, string expectedError)
         {
-            var userController = SetUserController(string.Empty);
+            StubAuthManager(userId);
 
-            var result = await userController.UpdatePassword(new PasswordUpdateModel());
+            var result = await _userController.UpdatePassword(passwordUpdateModel);
 
             var badRequestResult = result as BadRequestObjectResult;
             Assert.NotNull(badRequestResult);
-            var stringValue = badRequestResult.Value as string;
-            Assert.Equal("This method is unavailable.", stringValue);
-        }
-
-        [Fact]
-        public async void UpdatePassword_ReturnsBadRequest_NullPasswordUpdateModel()
-        {
-            var userController = SetUserController(Guid.NewGuid().ToString());
-
-            var result = await userController.UpdatePassword(null);
-
-            var badRequestResult = result as BadRequestObjectResult;
-            Assert.NotNull(badRequestResult);
-            var stringValue = badRequestResult.Value as string;
-            Assert.Equal("Not enough parameters to update password.", stringValue);
+            Assert.IsType<string>(badRequestResult.Value);
+            Assert.Equal(expectedError, badRequestResult.Value);
         }
 
         [Fact]
         public async void GetUserInfo_ReturnsUserModel()
         {
-            var userController = SetUserController(Guid.NewGuid().ToString());
+            StubAuthManager(Guid.NewGuid().ToString());
 
-            var result = await userController.GetUserInfo();
+            var result = await _userController.GetUserInfo();
 
             var okResult = result as OkObjectResult;
             Assert.NotNull(okResult);
-            var modelResult = okResult.Value as UserModel;
-            Assert.NotNull(modelResult);
+            Assert.IsType<UserModel>(okResult.Value);
+            Assert.NotNull(okResult.Value);
         }
 
         [Fact]
-        public async void GetUserInfo_ReturnsBadRequest_WrongToken()
+        public async void GetUserInfo_ReturnsBadRequest()
         {
-            var userController = SetUserController(string.Empty);
+            StubAuthManager(string.Empty);
 
-            var result = await userController.GetUserInfo();
+            var result = await _userController.GetUserInfo();
 
             var badRequestResult = result as BadRequestObjectResult;
             Assert.NotNull(badRequestResult);
-            var stringValue = badRequestResult.Value as string;
-            Assert.Equal("This method is unavailable.", stringValue);
+            Assert.IsType<string>(badRequestResult.Value);
+            Assert.Equal("This method is unavailable.", badRequestResult.Value);
         }
+
+
+        #region Helpers
+
+        public static IEnumerable<object[]> UpdateUser_ReturnsBadRequestTestData
+            => new[]
+            {
+                new object[] { string.Empty, new UserModel(), "This method is unavailable." },
+                new object[] { Guid.NewGuid().ToString(), null, "Not enough parameters to update user." }
+            };
+
+        public static IEnumerable<object[]> UpdatePassword_ReturnsBadRequestTestData
+            => new[]
+            {
+                new object[] { string.Empty, new PasswordUpdateModel(), "This method is unavailable." },
+                new object[] { Guid.NewGuid().ToString(), null, "Not enough parameters to update password." }
+            };
+
+        private void StubAuthManager(string userId)
+        {
+            var jwtToken = new JwtSecurityToken(claims: new Claim[]
+            {
+                new("id", userId)
+            });
+
+            A.CallTo(() => _authManager.DecodeJwtToken(A<string>._))
+                .Returns(jwtToken);
+        }
+
+        #endregion
     }
 }
